a384d76906ea9b9ca00b9aa57fff034b632c6f7b
Zytronium <dastelljes@outlook.com>
2025-08-08 17:14:57 -0500

    Render.com fix for SSL

--- src/config/database.js
+++ src/config/database.js
@@ -11,6 +11,7 @@ const pool = new Pool({
   database: process.env.DB_NAME || 'star_trek_db',
   password: process.env.DB_PASSWORD || '',
   port: process.env.DB_PORT || 5432,
+  ssl: { rejectUnauthorized: false },
 });
 
 
